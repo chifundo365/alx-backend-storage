d6cd35db09fe64d73134e538911a5388f9c7fe07
= <=>
2024-09-03 12:22:56 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- ists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan FROM metal_bands ORDER BY lifespan DESC;
+SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan FROM metal_bands WHERE COALESCE(split, 2022) <= 2022 ORDER BY lifespan DESC;
