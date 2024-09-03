5b180be0f5ee86ad3f2cff50156e411f76623dec
= <=>
2024-09-03 10:19:36 +0000

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
 -- Creates a table 'userrs' with id, email and name columns
 CREATE TABLE  IF NOT EXISTS users(
-	id INTEGER NOT NULL AUTO INCREMENT,
-	email STRING(255) NOT NULL, UNIQUE,
-	name STRING(255)
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY(id)
 )
