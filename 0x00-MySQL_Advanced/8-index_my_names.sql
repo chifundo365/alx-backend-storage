39b58a7fa2fc5294b514b6c8bdd4bc0b93fed084
= <=>
2024-09-05 17:04:51 +0000

    Optimise simple search

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,4 @@
 -- creates an index idx_name_first on the table names and the first letter of name.
-DROP INDEX IF EXISTS idx_name_first ON names(name(1));
+DROP INDEX IF EXISTS idx_name_first;
+CREATE INDEX idx_name_first ON names(name(1));
+
