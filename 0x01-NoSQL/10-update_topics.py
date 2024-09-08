7d866fe2410c2829dd46febdf2f4a7ef41ed7927
= <=>
2024-09-08 13:45:04 -0700

    chnage school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Updating a document in a collection '''
+
+
+def update_topics(mongo_collection, name, topics):
+    ''' Changes all topics of a school document based on the name '''
+    query = {'name': name}
+    values = { '$set': {'topics': topics}}
+    mongo_collection.update_one(query, values)
