09bf26db008887880a24923b32a678c5e4a3963b
= <=>
2024-09-09 23:34:31 -0700

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,37 +2,37 @@
 ''' Provides some stats about Nginx logs stored in MongoDB '''
 import pymongo
 
+if __name__ == '__main__':
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
+    total_docs = nginx_collection.count_documents({})
 
-total_docs = nginx_collection.count_documents({})
+    get = nginx_collection.count_documents({'method': 'GET'})
+    post = nginx_collection.count_documents({'method': 'POST'})
+    put = nginx_collection.count_documents({'method': 'PUT'})
+    patch = nginx_collection.count_documents({'method': 'PATCH'})
+    delete = nginx_collection.count_documents({'method': 'DELETE'})
 
-get = nginx_collection.count_documents({'method': 'GET'})
-post = nginx_collection.count_documents({'method': 'POST'})
-put = nginx_collection.count_documents({'method': 'PUT'})
-patch = nginx_collection.count_documents({'method': 'PATCH'})
-delete = nginx_collection.count_documents({'method': 'DELETE'})
+    methods = {
+            'GET': get,
+            'POST': post,
+            'PUT': put,
+            'PATCH': patch,
+            'DELETE': delete
+            }
 
-methods = {
-        'GET': get,
-        'POST': post,
-        'PUT': put,
-        'PATCH': patch,
-        'DELETE': delete
-        }
+    get_status = nginx_collection.count_documents(
+            {
+                'method': 'GET',
+                'path': '/status'
+            }
+            )
 
-get_status = nginx_collection.count_documents(
-        {
-            'method': 'GET',
-            'path': '/status'
-        }
-        )
+    print('{} logs'.format(total_docs))
+    print('Methods:')
 
-print('{} logs'.format(total_docs))
-print('Methods:')
+    for method, total in methods.items():
+        print('\tmethod {}: {}'.format(method, total))
 
-for method, total in methods.items():
-    print('\tmethod {}: {}'.format(method, total))
-
-print('{} status check'.format(get_status))
+    print('{} status check'.format(get_status))
