4427a495f5a779a5ea18120425f5c2042e1c2e47
= <=>
2024-10-30 12:33:52 +0000

    Writing strings to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Createsa redis connection """
 import redis
 from uuid import uuid4
-from typing import Any
+from typing import Union
 
 
 class Cache:
@@ -11,7 +11,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generates a key and sets the data as the value in redis server """
         key = str(uuid4())
         self._redis[key] = data
